7b5321f7fe956c2a9262e2b717306676d91257f8
bfeldman89 <26087892+bfeldman89@users.noreply.github.com>
2019-12-28 18:17:57 -0600

    Update common.py

--- common.py
+++ common.py
@@ -1,53 +1,37 @@
 #!/usr/bin/env /python
 """This module provides a function for shipping logs to Airtable."""
-
 import os
 import time
 from airtable import Airtable
 import cloudinary
 from documentcloud import DocumentCloud
+import tweepy
 
-airtab_intakes = Airtable(base_key=os.environ['jail_scrapers_db'],
-                          table_name='intakes',
-                          api_key=os.environ['AIRTABLE_API_KEY'])
-
-airtab_log = Airtable(base_key=os.environ['log_db'],
-                      table_name='log',
-                      api_key=os.environ['AIRTABLE_API_KEY'])
-
-airtab_daily = Airtable(base_key=os.environ['jail_scrapers_db'],
-                        table_name='daily stats',
-                        api_key=os.environ['AIRTABLE_API_KEY'])
-
-airtab_courts = Airtable(base_key=os.environ['other_scrapers_db'],
-                         table_name='courts',
-                         api_key=os.environ['AIRTABLE_API_KEY'])
-
-airtab_mdoc = Airtable(base_key=os.environ['other_scrapers_db'],
-                       table_name='mdoc',
-                       api_key=os.environ['AIRTABLE_API_KEY'])
-
-airtab_sos = Airtable(base_key=os.environ['other_scrapers_db'],
-                      table_name='exec_orders',
-                      api_key=os.environ['AIRTABLE_API_KEY'])
+airtab_homicides_by_cop = Airtable(os.environ['police_violence_db'], 'wapo', os.environ['AIRTABLE_API_KEY'])
 
 airtab_tweets = Airtable(base_key=os.environ['botfeldman89_db'],
                          table_name='scheduled_tweets',
                          api_key=os.environ['AIRTABLE_API_KEY'])
+airtab_log = Airtable(base_key=os.environ['log_db'],
+                      table_name='log',
+                      api_key=os.environ['AIRTABLE_API_KEY'])
 
-
-dc = DocumentCloud(username=os.environ['DOCUMENT_CLOUD_USERNAME'],
-                   password=os.environ['DOCUMENT_CLOUD_PW'])
+auth = tweepy.OAuthHandler(os.environ['TWITTER_APP_KEY'], os.environ['TWITTER_APP_SECRET'])
+auth.set_access_token(os.environ['TWITTER_OAUTH_TOKEN'], os.environ['TWITTER_OAUTH_TOKEN_SECRET'])
+tw = tweepy.API(auth)
 
 cloudinary.config(cloud_name='bfeldman89',
                   api_key=os.environ['CLOUDINARY_API_KEY'],
                   api_secret=os.environ['CLOUDINARY_API_SECRET'])
 
+dc = DocumentCloud(username=os.environ['DOCUMENT_CLOUD_USERNAME'],
+                   password=os.environ['DOCUMENT_CLOUD_PW'])
+
 muh_headers = {'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/53.0.2785.143 Safari/537.36'}
 
 
-my_funcs = {'wapo_fatal_shootings_by_ms_leos': 'recnCumWo19foxa3C'}
 
+my_funcs = {'wapo_fatal_shootings_by_ms_leos': 'recnCumWo19foxa3C'}
 
 def wrap_from_module(module):
     def wrap_it_up(t0, new=None, total=None, function=None):
