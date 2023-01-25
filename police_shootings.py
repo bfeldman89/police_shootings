abaa801e8046db3f5fceed15cb3db2782cc3c907
Blake Feldman <26087892+bfeldman89@users.noreply.github.com>
2023-01-25 05:23:52 -0600

    Update police_shootings.py

--- police_shootings.py
+++ police_shootings.py
@@ -13,32 +13,37 @@ def wapo_fatal_shootings_by_ms_leos():
     """This function does blah blah."""
     t0, i = time.time(), 0
     ms_list = []
-    url = 'https://raw.githubusercontent.com/washingtonpost/data-police-shootings/master/fatal-police-shootings-data.csv'
+    url = 'https://raw.githubusercontent.com/washingtonpost/data-police-shootings/master/v2/fatal-police-shootings-data.csv'
     with requests.Session() as s:
         r = s.get(url)
         data = r.content.decode('utf-8')
         csv_reader = csv.reader(data.splitlines(), delimiter=',')
         full_list = list(csv_reader)
         for row in full_list:
-            if row[9] == "MS":
+            if row[7] == "MS":
                 ms_list.append(row)
     print(f'total ms fatalities: {len(ms_list)}')
     for row in ms_list:
         this_dict = {}
         this_dict['id'] = row[0]
-        this_dict['name'] = row[1]
-        this_dict['date'] = row[2]
-        this_dict['manner_of_death'] = row[3]
-        this_dict['armed'] = row[4]
-        this_dict['age'] = row[5]
-        this_dict['gender'] = row[6]
-        this_dict['race'] = row[7]
-        this_dict['city'] = row[8]
-        this_dict['state'] = row[9]
-        this_dict['signs_of_mental_illness'] = row[10]
-        this_dict['threat_level'] = row[11]
-        this_dict['flee'] = row[12]
-        this_dict['body_camera'] = row[13]
+        this_dict['date'] = row[1]
+        this_dict['threat_type'] = row[2]
+        this_dict['flee_status'] = row[3]
+        this_dict['armed_with'] = row[4]
+        this_dict['city'] = row[5]
+        this_dict['county'] = row[6]
+        this_dict['state'] = row[7]
+        this_dict['latitude'] = row[8]
+        this_dict['longitude'] = row[9]
+        this_dict['location_precision'] = row[10]
+        this_dict['name'] = row[11]
+        this_dict['age'] = row[12]
+        this_dict['gender'] = row[13]
+        this_dict['race'] = row[14]
+        this_dict['race_source'] = row[15]
+        this_dict['was_mental_illness_related'] = row[16]
+        this_dict['body_camera'] = row[17]
+        this_dict['agency_ids'] = row[18]
         m = airtab.match('id', this_dict['id'])
         if m:
             airtab.update(m['id'], this_dict, typecast=True)
